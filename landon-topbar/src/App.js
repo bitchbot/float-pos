d5eeedd62b7a89a981c28e18add4c75a86a34fab
landon <lundquistlandon@gmail.com>
2021-11-17 02:06:06 +0100

    First commit

---
+++ landon-topbar/src/App.js
@@ -0,0 +1,12 @@
+import Topbar from "./components/Topbar/Topbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
+}
+
+export default App;
